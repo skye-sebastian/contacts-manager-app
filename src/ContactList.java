fe6f138e9d99a35b0782abdc9fd4dc9c4779370c
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-09-23 14:28:36 -0500

    Complete addContact functionality

--- src/ContactList.java
+++ src/ContactList.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ContactList {
@@ -10,11 +13,10 @@ public class ContactList {
     }
 
 
-    public static String readFromContactsList(){
-        Path contactsFile = Paths.get("src/contacts.txt");
-
+    public static String viewContacts(){
+        Path contactsFilePath = Paths.get("src/contacts.txt");
         try {
-            List<String> contactsList = Files.readAllLines(contactsFile);
+            List<String> contactsList = Files.readAllLines(contactsFilePath);
             for (int i = 0; i < contactsList.size(); i += 1) {
                 System.out.println((i + 1) + ": " + contactsList.get(i));
             }
@@ -22,9 +24,36 @@ public class ContactList {
         } catch (IOException ioe){
             ioe.printStackTrace();
         }
-
         return "";
     }
+
+    public static List<String> addContact(Contact contact) {
+        List<String> formattedContact = Arrays.asList(contact.getName() + " | " + contact.getPhoneNumber());
+        Path contactsFilePath = Paths.get("src/contacts.txt");
+        try {
+            Files.write(contactsFilePath, formattedContact, StandardOpenOption.APPEND);
+            return formattedContact;
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+        return formattedContact;
+
+    }
+
+    public static List<String> addContact(String name, String number) {
+        Contact contact = new Contact(name, number);
+        List<String> formattedContact = Arrays.asList(contact.getName() + " | " + contact.getPhoneNumber());
+        Path contactsFilePath = Paths.get("src/contacts.txt");
+        try {
+            Files.write(contactsFilePath, formattedContact, StandardOpenOption.APPEND);
+            return formattedContact;
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+        return formattedContact;
+    }
+
+
 
 
 
