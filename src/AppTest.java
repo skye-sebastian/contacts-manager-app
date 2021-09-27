a721f29d00310034eddc50d14b53a4d0f87adbf9
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-09-27 15:29:29 -0500

    Final functionality test

--- src/AppTest.java
+++ src/AppTest.java
@@ -5,23 +5,24 @@ public class AppTest {
     public static void main(String[] args) {
 
         ContactList contactList = new ContactList();
-//        Contact newContact = new Contact("test2", "0987654321");
+        Contact newContact = new Contact("test2", "0987654321");
 
-//        contactList.viewContacts();
-//        System.out.println("Adding new contact");
-//        contactList.addContact("Test", "1234567890");
-//        contactList.viewContacts();
+        contactList.viewContacts();
+
+        System.out.println("Adding new contact w/ string input");
+        contactList.addContact("Test", "1234567890");
+        contactList.viewContacts();
 //
-//        System.out.println("Adding new contact");
-//        contactList.addContact(newContact);
-//        contactList.viewContacts();
+        System.out.println("Adding new contact w/ object");
+        contactList.addContact(newContact);
+        contactList.viewContacts();
+        System.out.println("Searching contacts");
 
-//        System.out.println("Searching contacts");
-//        ContactList.searchContacts("Test");
+        contactList.searchContacts("test2");
 
 //        contactList.deleteContact("Test");
 
-        System.out.println(contactList);
+//        System.out.println(contactList);
 
     }
 
